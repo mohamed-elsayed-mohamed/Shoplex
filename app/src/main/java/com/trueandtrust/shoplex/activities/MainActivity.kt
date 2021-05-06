d41a4ba6cb836c37c0f517bc5470d22941718acc
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-05-06 02:33:30 +0200

    Initial commit

---
+++ app/src/main/java/com/trueandtrust/shoplex/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.trueandtrust.shoplex.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trueandtrust.shoplex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
