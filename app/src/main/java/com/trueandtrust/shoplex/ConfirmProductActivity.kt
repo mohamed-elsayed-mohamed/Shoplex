1d8670957af20ca85e3cfb4c1d83cc1a3e22b2db
Abeer Meckawi <76879741+abeerMeckawi@users.noreply.github.com>
2021-05-07 03:46:57 +0200

    Add and Confirm Product Design

---
+++ app/src/main/java/com/trueandtrust/shoplex/ConfirmProductActivity.kt
@@ -0,0 +1,16 @@
+package com.trueandtrust.shoplex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_product)
+    }
+}
+
+
+
+//        textview2.paintFlags = textview2.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+//        textview2.text = "2. StrikeThrough Using Paint Flags"
