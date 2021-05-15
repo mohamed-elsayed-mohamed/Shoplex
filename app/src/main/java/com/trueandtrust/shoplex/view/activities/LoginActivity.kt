b8955173844eeae5e143e5cf3cbdd3224bb1c4b2
Abeer Meckawi <76879741+abeerMeckawi@users.noreply.github.com>
2021-05-15 22:06:12 +0200

    commit

--- app/src/main/java/com/trueandtrust/shoplex/view/activities/LoginActivity.kt
+++ app/src/main/java/com/trueandtrust/shoplex/view/activities/LoginActivity.kt
@@ -2,11 +2,52 @@ package com.trueandtrust.shoplex.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.trueandtrust.shoplex.R
+import com.trueandtrust.shoplex.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(baseContext,"Login Successfully",Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
     }
 }
