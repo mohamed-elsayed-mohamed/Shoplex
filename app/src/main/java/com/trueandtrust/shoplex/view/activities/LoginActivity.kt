1b238e9e09fabee1d31257caa9e7e6bc0fe5dea6
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-05-15 20:31:25 +0200

    Update Login UI

---
+++ app/src/main/java/com/trueandtrust/shoplex/view/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.trueandtrust.shoplex.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trueandtrust.shoplex.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
