d9ea8c7e0c67d47dfdb9a10453ebb50989ff2211
Azhar-ragab <77209767+Azhar-ragab@users.noreply.github.com>
2021-05-21 23:26:05 +0200

    update

--- app/src/main/java/com/trueandtrust/shoplex/model/pojo/ChatHead.kt
+++ app/src/main/java/com/trueandtrust/shoplex/model/pojo/ChatHead.kt
@@ -2,21 +2,8 @@ package com.trueandtrust.shoplex.model.pojo
 
 data class ChatHead(val productId : String =" ",val storeId : String =" ",val chatId : String="",val productName:String="",val discountInfo : String="",val price: Double=0.0,val productImageURL : String ="",val userName : String="",val numOfMessage : Int=0) {
 
-    constructor(
-        productName: String,
-        price: Double,
-        productImageUrl: String,
-        userName: String,
-        numOfMessage: Int
-    ) {
-        this.productName = productName
-        this.price = price
-        this.productImageUrl = productImageUrl
-        this.userName = userName
-        this.numOfMessage = numOfMessage
-    }
 
-    constructor()
+
 
     fun getChatHeadsInfo() : ArrayList<ChatHead>{
         return arrayListOf()
