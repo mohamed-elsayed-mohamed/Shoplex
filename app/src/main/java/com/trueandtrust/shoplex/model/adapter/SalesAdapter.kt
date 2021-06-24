fbbd69f0491f3600ef52bcae7c8c58d15ff68287
Abeer Meckawi <76879741+abeerMeckawi@users.noreply.github.com>
2021-06-25 01:28:55 +0200

    theme

--- app/src/main/java/com/trueandtrust/shoplex/model/adapter/SalesAdapter.kt
+++ app/src/main/java/com/trueandtrust/shoplex/model/adapter/SalesAdapter.kt
@@ -1,11 +1,13 @@
 package com.trueandtrust.shoplex.model.adapter
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import com.trueandtrust.shoplex.R
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
