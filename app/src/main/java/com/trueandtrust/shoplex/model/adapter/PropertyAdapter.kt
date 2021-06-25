adc2261599e365befc2f5578210429a91bbcc71e
Azhar-ragab <azharragab9@gmail.com>
2021-06-25 03:03:41 +0200

    fix some feature

--- app/src/main/java/com/trueandtrust/shoplex/model/adapter/PropertyAdapter.kt
+++ app/src/main/java/com/trueandtrust/shoplex/model/adapter/PropertyAdapter.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.trueandtrust.shoplex.R
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 import com.trueandtrust.shoplex.databinding.PropertyItemRowBinding
+import com.trueandtrust.shoplex.model.extra.StoreInfo
 import com.trueandtrust.shoplex.model.pojo.Property
 
 class PropertyAdapter(val properties: ArrayList<Property>, val isViewOnly: Boolean = false) :
@@ -43,8 +48,26 @@ class PropertyAdapter(val properties: ArrayList<Property>, val isViewOnly: Boole
                 else {
                     chipItem = LayoutInflater.from(binding.root.context).inflate(R.layout.chip_entry_item, null, false) as Chip
                     binding.btnDelteProperty.setOnClickListener {
-                        properties.remove(property)
-                        notifyItemRemoved(bindingAdapterPosition)
+                        val builder = AlertDialog.Builder(context)
+                        builder.setTitle(context.getString(R.string.delete))
+                        builder.setMessage(context.getString(R.string.deleteMessage))
+
+                        builder.setPositiveButton(context.getString(R.string.yes)) { _, _ ->
+                            val snackbar = Snackbar.make(binding.root, binding.root.context.getString(R.string.deleteSuccess), Snackbar.LENGTH_LONG)
+                            val sbView: View = snackbar.view
+                            sbView.setBackgroundColor(ContextCompat.getColor(binding.root.context, R.color.blueshop))
+                            snackbar.show()
+                            properties.remove(property)
+                            notifyItemRemoved(bindingAdapterPosition)
+
+                        }
+
+                        builder.setNegativeButton(context.getString(R.string.no)) { dialog, _ ->
+                            dialog.cancel()
+                        }
+
+                        builder.show()
+
                     }
                 }
 
